3fe4028d8da503f6098048f24d5fc17f5f4488f3
Akash Maurya <47055948+akash99909@users.noreply.github.com>
2021-05-13 13:46:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React,{ Components } from 'react';
+import './App.css';
+class App extends Components{
+  render(){
+    return(
+    <div className="App">
+      <todos/>
+    </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/todos.js
@@ -0,0 +1,12 @@
+import React,{ Components } from 'react'
+
+class todos extends Components {
+    render(){
+        return (
+            <h1>Todo List</h1>
+        );
+      }
+    }
+  
+
+export default todos;
